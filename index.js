ffad244ab1d18bb2e839c239a9fe1c48318fcd08
Jesse Gonzalez <jessegonzalez024@gmail.com>
2020-03-06 03:39:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-function createEmployeeRecord(array,){
-  
+function createEmployeeRecord(array){
+    let obj = {
+      firstName: array[0], 
+      familyName: array[1],
+      title: array[2],
+      payPerHour: array[3],
+      timeInEvents: [],
+      timeOutEvents: []
+    }
 }
 
 function createEmployeeRecords(){
